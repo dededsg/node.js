345dffbd32140cc7f9ffac3dc9b413308b0c0d42
John <89491969+dededsg@users.noreply.github.com>
2023-04-28 16:39:04 -0300

    email top
    ta funfando

--- modulos/email/email.js
+++ modulos/email/email.js
@@ -1,24 +1,25 @@
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    user: 'youremail@gmail.com',
-    pass: 'yourpassword'
-  }
+    user: "develinsouzag@gmail.com",
+    pass: "ikvgjaqpfocgaxgb",
+  },
 });
 
+
 var mailOptions = {
-  from: 'youremail@gmail.com',
-  to: 'myfriend@yahoo.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+  from: "develinsouzag@gmail.com",
+  to: "gustavocrispimtec@gmail.com",
+  subject: "Sending Email using Node.js",
+  text: "",
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+transporter.sendMail(mailOptions, function (error, info) {
   if (error) {
     console.log(error);
   } else {
-    console.log('Email sent: ' + info.response);
+    console.log("Email sent: " + info.response);
   }
 });
