c19795ff7bc6d90d37e70a42d111c4022247661f
John <89491969+dededsg@users.noreply.github.com>
2023-05-10 17:32:53 -0300

    cadastro funcional

---
+++ cadastro/lista_clientes.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configurações do banco de dados
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'meuBanco'
+});
+
+// Conexão com o banco de dados
+connection.connect();
+
+// Rota para processar a consulta
+app.post('/clientes', (req, res) => {
+
+  // Consulta no banco de dados
+  connection.query(`SELECT * FROM clientes`, (error, results, fields) => {
+    if (error) throw error;
+    
+    // Exibição dos resultados
+    let html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Clientes</title>
+          <style>
+          table {
+            border-collapse: collapse;
+          }
+          
+          tr {
+            border: 1px solid black;
+          }
+          
+          th, td {
+            border: 1px solid black;
+            padding: 8px; 
+          }
+          </style>
+        </head>
+        <body>
+          <h1>Clientes encontrados</h1>
+          <table>
+            <tr>
+              <th>Nome</th>
+              <th>endereco</th>
+              <th>idade</th>
+              <th>sexo</th>
+            </tr>
+    `;
+    
+    results.forEach((cliente) => {
+      html += `
+        <tr>
+          <td>${cliente.nome}</td>
+          <td>${cliente.endereco}</td>
+          <td>${cliente.idade}</td>
+          <td>${cliente.sexo}</td>
+        </tr>
+      `;
+    });
+    
+    html += `
+          </table>
+        </body>
+      </html>
+    `;
+    
+    res.send(html);
+  });
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
